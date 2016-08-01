76c87e5ee30c4f6e8381efc3150525ef4ef266a0
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2016-07-31 23:28:14 -0400

    Update AndroidProducts.mk
    Added entry for AOSP

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,4 +1,5 @@
 LOCAL_PATH := device/samsung/gprimeltecan
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_gprimeltecan.mk \
-    $(LOCAL_DIR)/full_gprimeltecan.mk
+    $(LOCAL_DIR)/full_gprimeltecan.mk \
+    $(LOCAL_DIR)/aosp_gprimeltecan.mk
